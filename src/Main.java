abddcc007b90c6734499b5b29cdee4ca0ee13e0d
Neto test <s.e.lebedeva74@gmail.com>
2023-12-02 18:06:23 +0500

    Reformated code

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        // Объявляете переменные для входных данных и
-        // параметров программы: одну для хранения
-        // стоимости билета, другую для хранения количества
-        // рублей для одной бонусной мили
+        float ticketValue = 1686.45F;
+        short bonusValue = 21;
+        int bonusSum;
 
-        // Рассчитываете количество бонусных миль, используя
-        // значения заведённых переменных. Ответ сохраняете в
-        // новую переменную и выводите на экран
-
-        float ticket_value = 13686.45F;
-        short bonus_value = 21;
-        int bonus_sum;
-
-        bonus_sum = (int) ticket_value/bonus_value;
-        System.out.println(bonus_sum);
+        bonusSum = (int) ticketValue / bonusValue;
+        System.out.println(bonusSum);
     }
 }
